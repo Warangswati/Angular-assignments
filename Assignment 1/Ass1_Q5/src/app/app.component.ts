4987f5c3e77ade573eac43609e4790d13ebc04e7
Warangswati <33955018+Warangswati@users.noreply.github.com>
2018-07-27 10:34:29 +0530

    Add files via upload

---
+++ Assignment 1/Ass1_Q5/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<h1>  Marvellous Infosystem  </h1> <h2>   Educating for better tommorow </h2>',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Marvelous Infosystem!';
+}
